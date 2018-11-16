a93f6330d822d114d090abea1f6b4e970572e585
tyty1602 <37279775+tyty1602@users.noreply.github.com>
2018-11-16 14:19:16 -0800

    updates

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burgers_name) VALUES ('Chicken Burger');
+INSERT INTO burgers (burgers_name) VALUES ('Bacon Avocado Burger');
+INSERT INTO burgers (burgers_name) VALUES ('Hawaian Burger');
+INSERT INTO burgers (burgers_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burgers_name) VALUES ('Breakfast Burger');
